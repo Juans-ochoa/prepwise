87234caf94adb3af6e7bb537864b28ba1cf9288c
Juans <sebastian.ochoa.dev@gmail.com>
2025-03-28 16:31:00 +0100

    feat: Add api to vapi

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,12 @@
+import Agent from "@/components/Agent";
+
+const Page = () => {
+  return (
+    <>
+      <h3>Interview generation</h3>
+      <Agent userName="Juanse" userId="1" />
+    </>
+  );
+};
+
+export default Page;
